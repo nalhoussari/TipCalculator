1e3ab41250587cc34e78029c285ae4361ef41921
nalhoussari <nalhoussari@hotmail.com>
2017-06-09 10:52:14 -0700

    Creating a new project TipCalculator

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Noor Alhoussari on 2017-06-09.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Noor Alhoussari on 2017-06-09.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
